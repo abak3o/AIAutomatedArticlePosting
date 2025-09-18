9b0f05070352bcbc394cb657df2614bda9550b7f
abakeoX <abakeo.x@gmail.com>
2025-09-19 01:40:28 +0900

    deepsheekのAPIを叩けるようにした

--- src/ai.py
+++ src/ai.py
@@ -7,6 +7,7 @@ load_dotenv()
 PROMPT = os.getenv("PROMPT")
 INSTRUCTIONS = os.getenv("INSTRUCTIONS")
 
+
 def chatGPT() -> str:
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     client = OpenAI(api_key=os.getenv(OPENAI_API_KEY))
@@ -16,25 +17,50 @@ def chatGPT() -> str:
         instructions=INSTRUCTIONS,
         input=PROMPT,
     )
+
     print(response.output_text)
     with open("text.txt", mode="w", encoding="UTF-8") as f:
         f.write(response.output_text)
 
     return response.output_text
 
-
 def gemini() -> str:
     GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
     client = genai.Client(api_key=GOOGLE_API_KEY)
-
     response = client.models.generate_content(model="gemini-2.5-flash", contents=PROMPT)
+
     print(response.text)
     with open("text.txt", mode="w", encoding="UTF-8") as f:
         f.write(response.text)
 
     return response.text
 
+def deepsheek() -> str:
+    DEEPSHEEK_API_KEY = os.getenv("DEEPSHEEK_API_KEY")
+    client = OpenAI(base_url="https://openrouter.ai/api/v1", api_key=DEEPSHEEK_API_KEY)
+
+    completion = client.chat.completions.create(
+        extra_headers={
+            "HTTP-Referer": "<YOUR_SITE_URL>",
+            "X-Title": "<YOUR_SITE_NAME>",
+        },
+        model="deepseek/deepseek-r1:free",
+        messages=[
+            {
+                "role": "user",
+                # "content": "What is the meaning of life?"
+                "content": PROMPT,
+            }
+        ],
+    )
+    print(completion.choices[0].message.content)
+    with open("text.txt", mode="w", encoding="UTF-8") as f:
+        f.write(completion.choices[0].message.content)
+
+    return completion.choices[0].message.content
+
 
 if __name__ == "__main__":
     # chatGPT()
-    gemini()
+    # gemini()
+    deepsheek()
